82463ad60615eecc59b866a3085d49e3d04e65fb
Anton Tkachev <anton.tkachev@liberanda.com>
2024-04-24 18:44:46 +0400

    Add Complexity example

---
+++ Complexity/Complexity/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Complexity
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            var random = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                list[i] = random.Next(100);
+            }
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                for (var j = 0; j < list.Count; j++)
+                {
+                    if (j != i && list[j] == list[i])
+                    {
+                        list.Remove(j);
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+}
